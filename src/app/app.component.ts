e9aab40c5f1cee6513b455be28f1bd85481ac117
Sunpreet Singh <asthethi>
2025-02-23 17:05:39 +0530

    added header bar and side-navbar skeleton

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import {HeaderComponent} from './header/header.component';
+import {SideNavComponent} from './side-nav/side-nav.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, SideNavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
